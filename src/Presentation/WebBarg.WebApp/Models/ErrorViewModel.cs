2c006060e6b7c41bb770d84824ea549fdf24ae47
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-02-05 10:51:46 +0330

    Add project files.

---
+++ src/Presentation/WebBarg.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBarg.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
